a147b5fc64f016cd0f1deb7da37ebe5b514436fc
Aditya Attkan <138722584+Tymnspayce@users.noreply.github.com>
2023-11-29 13:39:48 +0530

    Just updated some contexts and file paths in the containers in react portfolio.
    1. Updated personal details in App.jsx.
    2. Updated skills and role in About.jsx.
    3. Updated social links in Footer.jsx
    4. Updated projects list in Portfolio.jsx.
    5. Updated Image paths and alttext in the Portfolio.jsx, Home.jsx and About.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/project-section.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "two projects collage which is mentioned in this section";
 
 /**
  * Project list
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "PassMyBook - An Initiative to grow education without financial limits",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Pass My Book is a great platform where you can Grow Emotionally and Mentally . This Web App lets you pass your used technologies books to those ones who have actual need of that.",
+    url: "https://passmybook--584aditya.repl.co/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "God_of_War_Fanverse",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "This is a website project for a game that inspires me to make this website for God of war Fans. This web app gather all the god of war fans at one place to share their thoughts and ideas about the game.",
+    url: "https://gowar.netlify.app/",
   },
 ];
 
